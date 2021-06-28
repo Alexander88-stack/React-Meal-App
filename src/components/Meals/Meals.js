0d1d4f230eaca9b76872a79d1803acc41274585c
Alexandru <>
2021-06-28 09:27:45 +0200

    first-look

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,19 @@
+
+
+import { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+
+
+
+const Meals = () => {
+    return (
+    <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+    );
+};
+
+export default Meals;
